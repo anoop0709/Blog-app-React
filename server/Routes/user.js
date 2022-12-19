b3c5611efe33c639ab2452a09f7ad471fdea5251
Anoop sukumaran nair <92984490+anoop0709@users.noreply.github.com>
2022-12-19 15:34:12 +0530

    Delete server directory

--- server/Routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {signIn,signUp} from "../controller/userController.js"
-const router = express.Router();
-
-router.post('/signin',signIn);
-router.post('/signup',signUp)
-
-
-export default router;
