c03524ccf244cbb4ac6455c30b0eedc54255a90b
Anoop sukumaran nair <92984490+anoop0709@users.noreply.github.com>
2022-12-19 15:33:58 +0530

    Delete client directory

--- client/index.d.ts
+++
@@ -1,1 +0,0 @@
-module 'react-file-base64';
--- client/src/Reducer/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers } from "redux";
-import posts from "./Posts"
-import { AuthReducer} from "./Auth"
-
-
-export default combineReducers({
-    posts,AuthReducer
-})
